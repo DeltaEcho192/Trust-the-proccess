7ac83c80f7b2fda0f1926c5e0afd1513c299b31d
25041134 <25041134@sun.ac.za>
2023-03-04 16:32:20 +0200

    Added background to cpu

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,52 @@
 use sysinfo::{CpuExt, System, SystemExt};
 use axum::{Router, Server, routing::get, extract::State, Json, response::IntoResponse};
 use std::sync::{Arc, Mutex};
+
 #[tokio::main]
 async fn main() {
+    let app_state = AppState::default();
     let router = Router::new()
         .route("/", get(root_get))
         .route("/api/cpus", get(cpu_get))
-        .with_state(AppState {sys: Arc::new(Mutex::new(System::new_all())) });
+        .with_state(app_state.clone());
+
+    tokio::task::spawn_blocking(move || {
+        let mut sys = System::new();
+        loop {
+            sys.refresh_cpu();
+            let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+
+            {
+                let mut cpus = app_state.cpus.lock().unwrap();
+                *cpus = v;
+            }
+
+            std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
+
+        }
+    });
+    
+
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Address of server is on {addr}");
     server.await.unwrap();
 }
 
-#[derive(Clone)]
+#[derive(Default, Clone)]
 struct AppState {
-    sys: Arc<Mutex<System>>,
+    cpus: Arc<Mutex<Vec<f32>>>,
 }
 
 
- async fn root_get(State(state): State<AppState>) -> String {
-     let mut sys = state.sys.lock().unwrap();
+ async fn root_get() -> String {
+     let mut sys = System::new();
      sys.refresh_all();
      let hostname = sys.host_name().unwrap();
      hostname
  }
 
  async fn cpu_get(State(state): State<AppState>) -> impl IntoResponse {
-     let mut sys = state.sys.lock().unwrap();
-     sys.refresh_cpu();
-
-     let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
-
+     let v = state.cpus.lock().unwrap().clone();
      Json(v)
  }
