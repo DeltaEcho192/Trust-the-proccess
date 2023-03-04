5f6d82f79db22df04e6d082150a64b0b727afc1f
25041134 <25041134@sun.ac.za>
2023-03-04 16:12:47 +0200

    Added hostname

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use sysinfo::{CpuExt, System, SystemExt};
-use axum::{Router, Server, routing::get, extract::State};
+use axum::{Router, Server, routing::get, extract::State, Json, response::IntoResponse};
 use std::sync::{Arc, Mutex};
 #[tokio::main]
 async fn main() {
     let router = Router::new()
         .route("/", get(root_get))
-        .with_state(AppState {sys: Arc::new(Mutex::new(System::new())) });
+        .route("/api/cpus", get(cpu_get))
+        .with_state(AppState {sys: Arc::new(Mutex::new(System::new_all())) });
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Address of server is on {addr}");
@@ -17,14 +18,19 @@ struct AppState {
     sys: Arc<Mutex<System>>,
 }
 
+
  async fn root_get(State(state): State<AppState>) -> String {
-     use std::fmt::Write;
-     let mut s = String::new();
+     let mut sys = state.sys.lock().unwrap();
+     sys.refresh_all();
+     let hostname = sys.host_name().unwrap();
+     hostname
+ }
+
+ async fn cpu_get(State(state): State<AppState>) -> impl IntoResponse {
      let mut sys = state.sys.lock().unwrap();
      sys.refresh_cpu();
-     for (i,cpu) in sys.cpus().iter().enumerate() {
-         let i = i + 1;
-         writeln!(&mut s, "CPU {i} {}%", cpu.cpu_usage()).unwrap();
-     }
-     s
+
+     let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+
+     Json(v)
  }
