e54ffb6a6d2148c88bc6eed90bce911cacfda998
25041134 <25041134@sun.ac.za>
2023-03-04 15:59:00 +0200

    Added sys info

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
-use axum::{Router, Server, routing::get};
+use sysinfo::{CpuExt, System, SystemExt};
+use axum::{Router, Server, routing::get, extract::State};
+use std::sync::{Arc, Mutex};
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root_get));
+    let router = Router::new()
+        .route("/", get(root_get))
+        .with_state(AppState {sys: Arc::new(Mutex::new(System::new())) });
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Address of server is on {addr}");
     server.await.unwrap();
 }
- async fn root_get() -> &'static str {
-     "Hello World"
+
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
+
+ async fn root_get(State(state): State<AppState>) -> String {
+     use std::fmt::Write;
+     let mut s = String::new();
+     let mut sys = state.sys.lock().unwrap();
+     sys.refresh_cpu();
+     for (i,cpu) in sys.cpus().iter().enumerate() {
+         let i = i + 1;
+         writeln!(&mut s, "CPU {i} {}%", cpu.cpu_usage()).unwrap();
+     }
+     s
  }
